c01389347fb28f504deb8f8e36299c97f6cbe3ef
TarouShota <adilov_02@mail.ru>
2024-03-30 03:56:07 +0100

    add eventReposity crud

---
+++ src/main/java/webCalendarSpring/demo/EventRepository.java
@@ -0,0 +1,19 @@
+package webCalendarSpring.demo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import webCalendarSpring.demo.Event;
+
+//@RepositoryRestResource(collectionResourceRel = "event", path = "event")
+public interface EventRepository extends CrudRepository<Event, Long> {
+
+    List<Event> findByDate(LocalDate date);
+
+    Event findById(long id);
+}
